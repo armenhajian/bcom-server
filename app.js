79521e983e2fb1724144bf944d9cde20f8f5c616
armenhajian <rmenhajian@gmail.com>
2017-04-21 08:59:01 +0400

    added server deploy mognodb credentials

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bcom');
+const serverConfig = JSON.parse(process.env.APP_CONFIG);
+const mongoPassword = '123456';
+const mongoURL = serverConfig ? "mongodb://" + serverConfig.mongo.user + ":" + mongoPassword + "@" +
+  serverConfig.mongo.hostString : 'mongodb://localhost:27017/bcom';
+console.log(serverConfig);
+console.log(mongoURL);
+mongoose.connect(mongoURL);
 mongoose.Promise = global.Promise;
 
 const Authorize = require('./services/Auth');
 
 const index = require('./routes/index');
-const auth  = require('./routes/auth');
+const auth = require('./routes/auth');
 const users = require('./routes/users');
 const posts = require('./routes/posts');
 const admin = require('./routes/admin');
@@ -27,10 +33,10 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   req.db = mongoose;
   next();
 });
@@ -40,17 +46,17 @@ app.use('/', index);
 app.use('/auth', auth);
 app.use('/users', Authorize, users);
 app.use('/posts', Authorize, posts);
-app.use('/admin',  admin);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -58,7 +64,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500).end();
 });
-
 
 
 module.exports = app;
